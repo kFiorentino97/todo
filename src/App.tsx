c5e3122606a603c3d86d66e34847ecfe83f083a1
kfiorentino97 <kurtfiorentino97@gmail.com>
2021-07-13 19:00:14 -0400

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@ionic/react/css/core.css';
+import { IonButton, IonDatetime, IonContent, IonLabel, IonInput, IonToggle, IonRadio, IonCheckbox, 
+         IonItemSliding, IonItemOption, IonItem, IonList, IonItemDivider} from '@ionic/react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <IonContent
+        color="primary">
+          <IonList
+          color="secondary">
+            <Task value = "Task #1"/>
+            <Task value = "Task #2"/>
+            <Task value = "Task #3"/>
+            <Task value = "Task #4"/>
+            <Task value = "Task #5"/>
+          </IonList>
+            <IonButton>Add</IonButton>
+            <IonButton>Remove</IonButton>
+        </IonContent>
       </header>
     </div>
   );
+}
+
+function Task(props: any){
+  return(
+    <IonItem>
+      <IonCheckbox slot="start" />
+      <IonLabel slot="start">{props.value}</IonLabel>
+      <IonButton slot="end" onClick={() => onDelete(props)}>Delete</IonButton>
+      <IonButton slot="end" onClick={() => onEdit(props)}>Edit</IonButton>
+    </IonItem>
+  )
+}
+
+function onDelete(props: any){
+  alert("Deleted!");
+}
+
+function onEdit(props: any){
+  alert("Edited!");
+}
+
+function onCheck(props: any){
+
 }
 
 export default App;
