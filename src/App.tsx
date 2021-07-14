b6e5675869b2aa25643c8bd372bb7ca360eada97
kfiorentino97 <kurtfiorentino97@gmail.com>
2021-07-14 13:11:13 -0400

    Updates: - Added support for localstorage - Added ability to edit - Added ability to add custom text

--- src/App.tsx
+++ src/App.tsx
@@ -23,91 +23,147 @@ function App() {
 }
 
 
-class TaskList extends React.Component<{}, {Tasks: number, messageText: String}> { // TODO: Change Tasks to an Array<String> from number. pass the array down so that the child can change it.
+
+
+class TaskList extends React.Component<{}, {Tasks: Array<string>, checklist: Array<boolean> ,messageText: string}> { // TODO: Change Tasks to an Array<String> from number. pass the array down so that the child can change it.
   /**
    * A class created to hold a list of tasks. Also contains an Add and Remove button
    */
   constructor(props: any){
     super(props);
+    let taskList: Array<string>;
+    let checkList: Array<boolean>;
+    if(localStorage.getItem("taskList") == null){
+      taskList = [];
+      localStorage.setItem("taskList", JSON.stringify(taskList));
+    }
+    else{
+      taskList = JSON.parse(localStorage.getItem("taskList")!);
+    }
+    if(localStorage.getItem("checkList") == null){
+      checkList = [];
+      localStorage.setItem("checkList", JSON.stringify(checkList));
+    }
+    else{
+      checkList = JSON.parse(localStorage.getItem("checkList")!);
+    }
     this.state = {
-      Tasks : 5,
-      messageText : "String"
+      Tasks: taskList,
+      checklist: checkList,
+      messageText : ""
     };
   }
 
-  private onAdd(){
-    this.setState({Tasks: this.state.Tasks + 1});
-  }
-
-  private onRemove(){
-    if(this.state.Tasks > 1){
-      this.setState({Tasks: this.state.Tasks - 1});
-    }
-  }
-
   render(){
-    /**
-     * This will loop and create a list of tasks the size of this.state.Tasks and will include an Add and Remove button at the bottom of the list.
-     */
     const tasklist: Array<React.ReactNode> = [];
-    for(var i = 0; i < this.state.Tasks; i++){
+    for(var i = 0; i < this.state.Tasks.length; i++){
       tasklist.push(
-        <Task tasks = {i + 1}/>
+        <Task tasks = {this.state.Tasks} checkList={this.state.checklist} index = {i} onUpdate = {() => this.onDelete()}/>
       )
     }
     return(
       <b>
-        {tasklist}
         <IonItem color="secondary">
-          <IonButton onClick={() => this.onAdd()}>Add</IonButton>
-          <IonButton onClick={() => this.onRemove()}>Remove</IonButton>
-          <IonInput placeholder="Enter text here." slot="end"/>
+          <IonButton slot="start" onClick={() => this.onAdd()}>Add</IonButton>
+          <IonButton slot="start" onClick={() => this.onRemove()}>Remove</IonButton>
+          <IonInput placeholder="Enter text here." value={this.state.messageText} slot="start"
+                    onIonChange={e => this.setText(e.detail.value!)}/>
         </IonItem>
+        {tasklist}
       </b>
     );
   }
+  
+  // Class methods:
+
+  private onAdd(){
+    this.state.Tasks.push(this.state.messageText);
+    this.state.checklist.push(false);
+    this.setState({});
+    localStorage.setItem("taskList", JSON.stringify(this.state.Tasks));
+  }
+
+  private onRemove(){
+    if(this.state.Tasks.length > 1){
+      this.state.Tasks.pop();
+      this.state.checklist.pop();
+      this.setState({});
+      localStorage.setItem("taskList", JSON.stringify(this.state.Tasks));
+    }
+  }
+
+  private onDelete(){
+    this.setState({});
+  }
+  
+  private setText(text: string){
+    this.setState({messageText: text});
+  }
 }
 
-class Task extends React.Component<{tasks: number}, {value: String}> {
+
+
+
+class Task extends React.Component<{tasks: Array<string>, checkList: Array<boolean>, index: number, onUpdate: any}, {}> {
+
+  inEditMode: boolean = false;
+  
   constructor(props: any){
     super(props);
-    this.state = {
-      value : "",
-    };
   }
 
   render(){
-    return(
-      <IonItem>
-        <IonCheckbox slot="start" />
-        <IonLabel slot="start">{"Task #" + this.props.tasks}</IonLabel>
-        <IonButton slot="end" onClick={() => this.onDelete()}>Delete</IonButton>
-        <IonButton slot="end" onClick={() => this.onEdit()}>Edit</IonButton>
-      </IonItem>
-    )
+    if(!this.inEditMode){
+      return(
+        <IonItem>
+          <IonCheckbox slot="start" checked={this.props.checkList[this.props.index]} onClick={() => this.onCheck()}/>
+          <IonLabel slot="start">{this.props.tasks[this.props.index]}</IonLabel>
+          <IonButton slot="end" onClick={() => this.onDelete()}>Delete</IonButton>
+          <IonButton slot="end" onClick={() => this.onEdit()}>Edit</IonButton>
+        </IonItem>
+      )
+    }
+    else{
+      return(
+        <IonItem>
+          <IonInput placeholder="Enter text here." value={this.props.tasks[this.props.index]} slot="start"
+          onIonChange={e => this.editText(e.detail.value!)}/>
+          <IonButton slot="end" onClick={() => this.onDone()}>Done</IonButton>
+        </IonItem>
+      )
+    }
   }
 
+  // Class methods: 
+
   onDelete(){
-    alert("Deleted!");
+    this.props.tasks.splice(this.props.index, 1);
+    this.props.checkList.splice(this.props.index, 1);
+    this.props.onUpdate();
+    localStorage.setItem("taskList", JSON.stringify(this.props.tasks));
   }
 
   onEdit(){
-    this.setState({value: "edited"});
+    //this.props.tasks[this.props.index] = "Edited!";
+    this.inEditMode = !this.inEditMode;
+    this.props.onUpdate();
   }
 
-}
+  editText(text: string){
+    this.props.tasks[this.props.index] = text;
+  }
 
+  onDone(){
+    this.inEditMode = !this.inEditMode;
+    this.props.onUpdate();
+    localStorage.setItem("taskList", JSON.stringify(this.props.tasks));
+  }
 
-function onDelete(props: any){
-  alert("Deleted!");
-}
-
-function onEdit(props: any){
-  props.value = "edited";
-}
-
-function onCheck(props: any){
-
+  onCheck(){
+    this.props.checkList[this.props.index] = !this.props.checkList[this.props.index];
+    this.props.onUpdate();
+    localStorage.setItem("checkList", JSON.stringify(this.props.checkList));
+  }
 }
 
 export default App;
